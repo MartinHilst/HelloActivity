b2f151b3f5b596a6b0ef24316b705d6f745f9ca7
martinhilst <martinhilst@gmail.com>
2025-03-17 22:12:03 -0300

    First Commit

--- app/src/main/java/martinhilst/com/github/HelloActivity/MainActivity.kt
+++ app/src/main/java/martinhilst/com/github/HelloActivity/MainActivity.kt
@@ -15,29 +15,6 @@ import martinhilst.com.github.HelloActivity.ui.theme.HelloActivityTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            HelloActivityTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HelloActivityTheme {
-        Greeting("Android")
     }
 }
